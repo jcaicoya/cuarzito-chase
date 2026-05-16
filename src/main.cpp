eb1a2ae518a49446bde56dce7832b98272b7907e
Caico <caicoya@gmail.com>
2026-05-16 14:14:06 +0200

    Accept orchestrator launch flags in cuarzito race

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <QApplication>
 #include <QCommandLineOption>
 #include <QCommandLineParser>
+#include <QScreen>
 #include <QSurfaceFormat>
+#include <QWindow>
 #include <csignal>
 #include "GameLaunchOptions.h"
 #include "MainWindow.h"
@@ -28,6 +30,19 @@ int main(int argc, char *argv[])
                                   "Run a named automated test track.",
                                   "name");
     parser.addOption(testOption);
+    QCommandLineOption demoOption(QStringList() << "demo",
+                                  "Launch in demo mode.");
+    parser.addOption(demoOption);
+    QCommandLineOption liveOption(QStringList() << "live",
+                                  "Launch in live mode.");
+    parser.addOption(liveOption);
+    QCommandLineOption fullscreenOption(QStringList() << "fullscreen",
+                                        "Launch in fullscreen mode.");
+    parser.addOption(fullscreenOption);
+    QCommandLineOption screenOption(QStringList() << "screen",
+                                    "Target screen index for fullscreen mode.",
+                                    "index");
+    parser.addOption(screenOption);
     parser.process(a);
 
     GameLaunchOptions options;
@@ -48,6 +63,28 @@ int main(int argc, char *argv[])
     }
 
     MainWindow w(options);
-    w.showFullScreen();
+    bool wantFullscreen = parser.isSet(fullscreenOption) || !parser.isSet(screenOption);
+    if (parser.isSet(screenOption)) {
+        bool ok = false;
+        int screenIndex = parser.value(screenOption).toInt(&ok);
+        if (ok) {
+            const auto screens = QGuiApplication::screens();
+            if (screenIndex >= 0 && screenIndex < screens.size()) {
+                if (QWindow* handle = w.windowHandle())
+                    handle->setScreen(screens[screenIndex]);
+                else {
+                    w.winId();
+                    if (QWindow* createdHandle = w.windowHandle())
+                        createdHandle->setScreen(screens[screenIndex]);
+                }
+                w.setGeometry(screens[screenIndex]->geometry());
+            }
+        }
+    }
+
+    if (wantFullscreen)
+        w.showFullScreen();
+    else
+        w.show();
     return a.exec();
 }
